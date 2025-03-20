19bd302d8ef4920156c956c17bff09bd5aacfca0
Xuemin Guan <xueminguan@macbookpro.lan>
2025-03-20 12:26:56 +0000

    add edit route

--- app/root.tsx
+++ app/root.tsx
@@ -22,7 +22,6 @@ export function HydrateFallback() {
 import { createEmptyContact } from "./data";
 
 export async function action() {
-    console.log("action is called")
     const contact = await createEmptyContact();
     return { contact };
 }
--- app/routes.ts
+++ app/routes.ts
@@ -4,8 +4,13 @@ import {route} from "@react-router/dev/routes";
 export default [
     layout("layouts/sidebar.tsx", [
             index("routes/home.tsx"),
-            route("contacts/:contactId", "routes/contact.tsx")
+            route("contacts/:contactId", "routes/contact.tsx"),
+            route(
+                "contacts/:contactId/edit",
+                "routes/edit-contact.tsx"
+            ),
         ]
     ),
     route("about", "routes/about.tsx"),
+
 ] satisfies RouteConfig;

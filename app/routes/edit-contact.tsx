b0e7f0c8c831215e81cecbd9149b890d26ea47fd
Xuemin Guan <xueminguan@macbookpro.lan>
2025-03-24 15:59:28 +0000

    added action to update contacts.

--- app/routes/edit-contact.tsx
+++ app/routes/edit-contact.tsx
@@ -1,8 +1,14 @@
-import { Form } from "react-router";
-import type { Route } from "./+types/edit-contact";
+import {Form, redirect} from "react-router";
+import type {Route} from "./+types/edit-contact";
+import {getContact, updateContact} from "../data";
 
-import { getContact } from "../data";
 
+export async function action({params, request }: Route.ActionArgs) {
+    const formData = await request.formData();
+    const updates = Object.fromEntries(formData);
+     await updateContact(params.contactId, updates);
+     return redirect(`/contacts/${params.contactId}`);
+}
 export async function loader({ params }: Route.LoaderArgs) {
     const contact = await getContact(params.contactId);
     if (!contact) {

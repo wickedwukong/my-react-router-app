8ff1329b3abb717fefd2826febd06c41ff013a80
Xuemin Guan <xueminguan@Xuemins-MacBook-Pro.local>
2025-04-17 09:26:23 +0100

    Made Cancel button work: https://reactrouter.com/tutorials/address-book#cancel-button Next: Url Search Params and GET submissions https://reactrouter.com/tutorials/address-book#urlsearchparams-and-get-submissions

--- app/routes/edit-contact.tsx
+++ app/routes/edit-contact.tsx
@@ -1,4 +1,4 @@
-import {Form, redirect} from "react-router";
+import {Form, redirect, useNavigate, useNavigation} from "react-router";
 import type {Route} from "./+types/edit-contact";
 import {getContact, updateContact} from "../data";
 
@@ -21,6 +21,7 @@ export default function EditContact({
                                         loaderData,
                                     }: Route.ComponentProps) {
     const { contact } = loaderData;
+    const navigate = useNavigate();
 
     return (
         <Form key={contact.id} id="contact-form" method="post">
@@ -69,7 +70,7 @@ export default function EditContact({
             </label>
             <p>
                 <button type="submit">Save</button>
-                <button type="button">Cancel</button>
+                <button type="button" onClick={() => navigate(-1)}>Cancel</button>
             </p>
         </Form>
     );

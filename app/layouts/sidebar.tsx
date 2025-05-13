e5beaeb3dc396ca7a2e0512183038d2ea340ddc4
Xuemin Guan <xueminguan@Xuemins-MacBook-Pro.local>
2025-05-13 11:08:24 +0100

    Made it work: synchronize urls to form state https://reactrouter.com/tutorials/address-book#synchronizing-urls-to-form-state Next: https://reactrouter.com/tutorials/address-book#submitting-forms-onchange submitting-forms-onchange

--- app/layouts/sidebar.tsx
+++ app/layouts/sidebar.tsx
@@ -1,21 +1,28 @@
 import {Form, Link, NavLink, Outlet, useNavigation} from "react-router";
 import {getContacts} from "../data";
 import type {Route} from "./+types/sidebar";
+import {useEffect} from "react";
 
 export async function loader({request}: Route.LoaderArgs) {
     console.log(request);
     const url = new URL(request.url);
     const q = url.searchParams.get("q");
     const contacts = await getContacts(q);
-    return {contacts};
+    return {contacts, q};
 }
 
 
 export default function SidebarLayout({
                                           loaderData,
                                       }: Route.ComponentProps) {
-    const {contacts} = loaderData;
+    const {contacts, q} = loaderData;
     const navigation = useNavigation();
+    useEffect(() => {
+        const searchField = document.getElementById("q");
+        if (searchField instanceof HTMLInputElement) {
+            searchField.value = q || "";
+        }
+    }, [q]);
 
     return (
         <>
@@ -31,6 +38,7 @@ export default function SidebarLayout({
                             name="q"
                             placeholder="Search"
                             type="search"
+                            defaultValue={q || ""}
                         />
                         <div
                             aria-hidden
